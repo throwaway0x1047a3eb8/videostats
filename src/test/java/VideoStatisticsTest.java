c5b6a96fef2a711a6f261c884717df75cdd66f36
Ivan Soares <ivansoares@Ivans-MacBook-Pro.local>
2019-09-11 12:38:01 -0300

    Adding VideoStatistics test.

---
+++ src/test/java/VideoStatisticsTest.java
@@ -0,0 +1,152 @@
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertThrows;
+
+public class VideoStatisticsTest {
+    private static final double DELTA = 0.00001;
+
+    @Test
+    public void testEmpty() {
+        VideoStatistics.DateService dateService = () -> 61000;
+        VideoStatistics stats = new VideoStatistics(dateService);
+        VideoStatistics.Result result = stats.get(60666);
+        assertEquals(result.count, 0);
+        assertEquals(result.sum, 0, DELTA);
+        assertEquals(result.avg, 0, DELTA);
+        assertEquals(result.max, 0, DELTA);
+        assertEquals(result.min, 0, DELTA);
+    }
+
+    @Test
+    public void testOldTimestamp() {
+        VideoStatistics.DateService dateService = () -> 61000;
+
+        VideoStatistics stats = new VideoStatistics(dateService);
+
+        assertThrows(IllegalArgumentException.class, () -> {
+            stats.post(3, 0);
+        });
+    }
+
+    @Test
+    public void testSinglePost() {
+        VideoStatistics.DateService dateService = () -> 61000;
+        VideoStatistics stats = new VideoStatistics(dateService);
+
+        try {
+            stats.post(10.5, 60666);
+        } catch (IllegalArgumentException ignored) {
+        }
+
+        VideoStatistics.Result result = stats.get(61666);
+        assertEquals(result.count, 1);
+        assertEquals(result.sum, 10.5, DELTA);
+        assertEquals(result.avg, 10.5, DELTA);
+        assertEquals(result.max, 10.5, DELTA);
+        assertEquals(result.min, 10.5, DELTA);
+    }
+
+    @Test
+    public void testOldEntry() {
+        VideoStatistics.DateService dateService = () -> 61000;
+        VideoStatistics stats = new VideoStatistics(dateService);
+
+        try {
+            stats.post(10.5, 60666);
+        } catch (IllegalArgumentException ignored) {
+        }
+
+        VideoStatistics.Result result = stats.get(121666);
+        assertEquals(result.count, 0);
+        assertEquals(result.sum, 0, DELTA);
+        assertEquals(result.avg, 0, DELTA);
+        assertEquals(result.max, 0, DELTA);
+        assertEquals(result.min, 0, DELTA);
+    }
+
+    @Test
+    public void testMultiplePosts() {
+        VideoStatistics.DateService dateService = () -> 61000;
+        VideoStatistics stats = new VideoStatistics(dateService);
+
+        try {
+            stats.post(5, 60666);
+            stats.post(25, 60667);
+            stats.post(125, 60668);
+        } catch (IllegalArgumentException ignored) {
+        }
+
+        VideoStatistics.Result result = stats.get(60669);
+        assertEquals(result.count, 3);
+        assertEquals(result.sum, 155, DELTA);
+        assertEquals(result.avg, 51.666666666, DELTA);
+        assertEquals(result.max, 125, DELTA);
+        assertEquals(result.min, 5, DELTA);
+    }
+
+    @Test
+    public void testMinMax() {
+        VideoStatistics.DateService dateService = () -> 61000;
+        VideoStatistics stats = new VideoStatistics(dateService);
+
+        try {
+            stats.post(5, 60666);
+            stats.post(1, 60667);
+            stats.post(25, 60668);
+            stats.post(625, 60669);
+            stats.post(125, 60670);
+        } catch (IllegalArgumentException ignored) {
+        }
+
+        VideoStatistics.Result result = stats.get(60669);
+        // Ensures min/max values are updated accordingly.
+        assertEquals(result.max, 625, DELTA);
+        assertEquals(result.min, 1, DELTA);
+    }
+
+    @Test
+    public void testCollision() {
+        VideoStatistics.DateService dateService = () -> 61000;
+        VideoStatistics stats = new VideoStatistics(dateService);
+
+        try {
+            stats.post(5, 60666);
+            // Happens 1 minute after the previous post call.
+            // Collides with it and must replace it.
+            stats.post(25, 120666);
+        } catch (IllegalArgumentException ignored) {
+        }
+
+        VideoStatistics.Result result = stats.get(60669);
+        assertEquals(result.count, 1);
+        assertEquals(result.sum, 25, DELTA);
+        assertEquals(result.avg, 25, DELTA);
+        assertEquals(result.max, 25, DELTA);
+        assertEquals(result.min, 25, DELTA);
+    }
+
+    @Test
+    public void testDelete() {
+        VideoStatistics.DateService dateService = () -> 61000;
+        VideoStatistics stats = new VideoStatistics(dateService);
+
+        try {
+            stats.post(5, 60666);
+            stats.post(25, 60667);
+            stats.post(125, 60668);
+            stats.post(25, 120666);
+        } catch (IllegalArgumentException ignored) {
+        }
+
+        // None of the post calls above should matter.
+        stats.delete();
+
+        VideoStatistics.Result result = stats.get(60669);
+        assertEquals(result.count, 0);
+        assertEquals(result.sum, 0, DELTA);
+        assertEquals(result.avg, 0, DELTA);
+        assertEquals(result.max, 0, DELTA);
+        assertEquals(result.min, 0, DELTA);
+    }
+}
