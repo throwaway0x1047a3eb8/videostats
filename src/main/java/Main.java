956e0222e6247b09ea01deae79e9f795fe8aeb84
Ivan Soares <ivansoares@Ivans-MacBook-Pro.local>
2019-09-11 10:03:22 -0300

    Initial commit.

---
+++ src/main/java/Main.java
@@ -0,0 +1,40 @@
+import com.google.gson.Gson;
+
+import java.util.Date;
+
+import static spark.Spark.*;
+
+public class Main {
+    public static class Video {
+        double duration;
+        long timestamp;
+    }
+
+    public static void main(String[] args) {
+        VideoStatistics videoStatistics = new VideoStatistics(() -> new Date().getTime());
+
+        post("/videos", (request, response) -> {
+            Video video = new Gson().fromJson(request.body(), Video.class);
+            try {
+                videoStatistics.post(video.duration, video.timestamp);
+            } catch (IllegalArgumentException e) {
+                response.status(204);
+                return "";
+            }
+
+            response.status(201);
+            return "";
+        });
+
+        delete("/videos", (request, response) -> {
+            videoStatistics.delete();
+
+            response.status(204);
+            return "";
+        });
+
+        get("/statistics", (request, response) -> {
+            return new Gson().toJson(videoStatistics.get(new Date().getTime()));
+        });
+    }
+}
---
+++ src/main/java/VideoStatistics.java
@@ -0,0 +1,95 @@
+public class VideoStatistics {
+    private static class Second {
+        double sum;
+        long count;
+        double max;
+        double min;
+
+        int timestamp;
+    }
+
+    public static class Result {
+        double sum;
+        double avg;
+        long count;
+        double max;
+        double min;
+    }
+
+    public interface DateService {
+        long getTime();
+    }
+
+    private Second[] seconds;
+
+    private DateService dateService;
+
+    public VideoStatistics(DateService dateService) {
+        this.dateService = dateService;
+
+        seconds = new Second[60];
+        for (int i = 0; i < 60; i++) {
+            seconds[i] = new Second();
+            seconds[i].max = 0;
+            seconds[i].min = Double.MAX_VALUE;
+        }
+    }
+
+    public void post(double duration, long timestamp) throws IllegalArgumentException {
+        int timestampSeconds = (int) (timestamp / 1000);
+        int nowSeconds = (int) (dateService.getTime() / 1000);
+
+        if (nowSeconds - timestampSeconds > 60) {
+            throw new IllegalArgumentException("Timestamp older than 60 seconds.");
+        }
+
+        Second second = seconds[timestampSeconds % 60];
+        if (second.timestamp != timestampSeconds) {
+            second.sum = 0;
+            second.count = 0;
+            second.max = 0;
+            second.min = Double.MAX_VALUE;
+        }
+
+        second.timestamp = timestampSeconds;
+        second.sum += duration;
+        second.count += 1;
+        second.max = Math.max(second.max, duration);
+        second.min = Math.min(second.min, duration);
+    }
+
+    public void delete() {
+        for (int i = 0; i < 60; i++) {
+            Second second = seconds[i];
+            second.sum = 0;
+            second.count = 0;
+            second.max = 0;
+            second.min = Double.MAX_VALUE;
+        }
+    }
+
+    public Result get(long now) {
+        int nowSeconds = (int) (now / 1000);
+
+        Result statistics = new Result();
+        statistics.max = 0;
+        statistics.min = Double.MAX_VALUE;
+
+        for (int i = 0; i < 60; i++) {
+            Second second = seconds[i];
+
+            if (nowSeconds - second.timestamp > 60) continue;
+
+            statistics.sum += second.sum;
+            statistics.count += second.count;
+            statistics.max = Math.max(statistics.max, second.max);
+            statistics.min = Math.min(statistics.min, second.min);
+        }
+
+        statistics.avg = statistics.count == 0 ? 0 : statistics.sum / statistics.count;
+        statistics.min = statistics.min == Double.MAX_VALUE ? 0 : statistics.min;
+
+        return statistics;
+
+    }
+}
