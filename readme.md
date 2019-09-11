e4f8ad805e8de2cc21229283850f13fa7c97ab4f
Ivan Soares <ivansoares@Ivans-MacBook-Pro.local>
2019-09-11 14:39:33 -0300

    Updating readme.md and test script

--- readme.md
+++ readme.md
@@ -8,3 +8,13 @@ $ mvn test
 $ mvn clean install
 $ java -jar target/videostats-1.0-jar-with-dependencies.jar
 
+  * You can now view the statistics at http://127.0.0.1:4567/statistics
+
+  * You can also post to the API or call delete using the "test.py" script.
+    * Example (with duration=30.5)
+      $ python test.py 30.5
+
+    * Example (delete)
+      $ python test.py --delete
+
+    * Obs: the script depends on requests>=2.22.0.
--- test.py
+++ test.py
@@ -2,9 +2,15 @@ import sys
 import requests
 from datetime import datetime
 
+URL = 'http://127.0.0.1:4567/videos'
+
+if len(sys.argv[1]) > 1 and sys.argv[1] == '--delete':
+    requests.delete(URL)
+    sys.exit(0)
+
 now = int(str(datetime.timestamp(datetime.now())).replace('.', '')[:13])
 
-# Change the timestamp to 55s ago to debug.
+# Change the timestamp to 55s ago (the entry will only last 5s).
 now = now - 55000
 
 duration = float(sys.argv[1]) if len(sys.argv) > 1 else 10.5
